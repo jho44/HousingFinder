85962f238c7ba786ac2b31ccaf5c328598c8733c
Jessica Ho <jho@Jessicas-MBP.attlocal.net>
2023-11-11 11:12:48 -0800

    include lib/api

---
+++ lib/api/getPosts.ts
@@ -0,0 +1,14 @@
+import { collection } from './mongo.ts';
+
+const PAGE_SIZE = 20;
+export default async function getPosts(pageNum: number) {
+  const query = collection
+    .find({})
+    .sort({ updated_at: -1 })
+    .limit(PAGE_SIZE)
+    .skip(pageNum * PAGE_SIZE)
+    .project({
+      _id: 0,
+    })
+  return await query.toArray();
+}
